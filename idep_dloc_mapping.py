58274afc01e4365f1e4b95cdb0b2fe22596a463f
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2020-12-01 16:59:36 -0800

    Update idep_dloc_mapping.py

--- idep_dloc_mapping.py
+++ idep_dloc_mapping.py
@@ -28,6 +28,7 @@ df['Rights Statement'] = ('This item was contributed to the Digital Library of t
 # tidy up columns
 df['dc.date'] = df['dc.date'].str.split(',', n = 1).str[0]
 df['mods_originInfo_place_placeTerm_ms'] = df['mods_originInfo_place_placeTerm_ms'].str.split(r'\\', n = 1).str[0]
+df['mods_titleInfo_title_ms'] = df['mods_titleInfo_title_ms'].str.split(r'\\', n = 1).str[0]
 df['mods_identifier_local_ms'] = df['mods_identifier_local_ms'].str.split(',', n = 1).str[-1]
 df['mods_part_detail_volume_number_ms'] = df['mods_part_detail_volume_number_ms'].str.split('.', n = 1).str[-1]
 df['mods_part_detail_issue_number_ms'] = df['mods_part_detail_issue_number_ms'].str.split('.', n = 1).str[-1]
