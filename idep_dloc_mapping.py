efe05aea8f42f9d7fc15b8d6b0f58a5ee08c7b8b
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2020-12-02 18:02:30 -0800

    Update idep_dloc_mapping.py

--- idep_dloc_mapping.py
+++ idep_dloc_mapping.py
@@ -52,7 +52,7 @@ df['Holding location statement'] = 'Newspapers are held at the ' + df['Holding l
 df['Holding location statement'] = df['Holding location statement'].astype(str) + ' in Havana, Cuba'
 
 
-#separate out the subjects into their own respective columns
+# separate out the subjects into their own respective columns
 df = df.join(df['Subject'].str.split(',', expand = True).add_prefix('Subject'))
 df.drop(['Subject'], axis = 1, inplace = True)
 df.columns = df.columns.str.replace('[0-9]', '') 
